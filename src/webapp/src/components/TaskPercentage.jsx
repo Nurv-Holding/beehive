611cb25fa8a1e330d57e7c5a68dad6c515727e96
Jefferson Dias <jefer.ld@hotmail.com>
2022-10-31 20:47:59 -0300

    webapi - connection database adjustment

--- src/webapp/src/components/TaskPercentage.jsx
+++ src/webapp/src/components/TaskPercentage.jsx
@@ -11,7 +11,7 @@ const TaskPercentage = ({returnQuantify, idTask, total, totalDone, returnQuantif
     },[idTask])
 
     const totalQuantify = total.filter(x => x?.idTask === idTask).map(x => x?.totalSubtasks)[0]
-    const totalQuantifyDone = totalDone.filter(x => x?.idTask === idTask).map(x => x?.totalSubtasksDone)[0] || 0
+    const totalQuantifyDone = totalDone.filter(x => x?.idTask === idTask).map(x => x?.totalSubtasksDone)[0]|| 0
 
     return (
         <span> {calcPercentage(totalQuantifyDone, totalQuantify)|| 0}% concluído</span>
