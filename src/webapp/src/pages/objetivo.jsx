c8fbdc443f324930a71dc8682c3de20c8ba884a4
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2022-11-01 16:37:57 -0300

    webapp - objetivo

--- src/webapp/src/pages/objetivo.jsx
+++ src/webapp/src/pages/objetivo.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Tab } from '@headlessui/react'
 import { useState } from 'react';
 import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
@@ -9,19 +10,20 @@ import TeamObjectivesNewTask from '../components/teamObjectivesNewTask';
 import TeamObjectivesPercentage from '../components/teamObjectivesPercentage';
 import TeamObjectivesTable from '../components/teamObjectivesTable';
 import { ContextUser } from '../context/ContextUser';
+import TaskList from '../components/empresasTabPanels/tarefas/listaTarefas';
 
 function Objetivo() {
   const { tasksToGoal,
     tasksToGoalQuantify,
     tasksToGoalQuantifyDone,
     modelChange,
-    item 
+    item
   } = useContext(ContextUser)
 
   const [total, setTotal] = useState([])
   const [totalDone, setTotalDone] = useState([])
   const [message, setMessage] = useState("Aqui vai uma mensagem")
-  const {idGoal} = useParams()
+  const { idGoal } = useParams()
 
   const returnQuantify = async () => {
     const { data } = await subtasksApi.getAllTaskQuantifySubtasks()
@@ -38,15 +40,14 @@ function Objetivo() {
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    if(Object.keys(item).length === 0 && 
-    item.name === "" || item.descriptions === "" || 
-    item.initialDate === "" || item.finalDate === "")
-    {
-        setMessage("Precisa preencher os campos vazios")
+    if (Object.keys(item).length === 0 &&
+      item.name === "" || item.descriptions === "" ||
+      item.initialDate === "" || item.finalDate === "") {
+      setMessage("Precisa preencher os campos vazios")
 
-    }else{
-        console.log("item",item)
-        tasksApi.create({...item, idGoal})
+    } else {
+      console.log("item", item)
+      tasksApi.create({ ...item, idGoal })
         .then(() => setMessage("Cadastro Realizado!"))
         .catch(() => setMessage("Algo deu errado!!"))
     }
@@ -56,30 +57,73 @@ function Objetivo() {
     <>
       <Header />
 
-      <main>
-        <div className='flex flex-col items-center'>
-          <h1 className='text-white text-bold text-2xl mt-8'> {tasksToGoalQuantify.nameGoal} </h1>
-          <div className='container-two-percentage'>
-            <TeamObjectivesPercentage
-              tasksToGoalQuantify={tasksToGoalQuantify}
-              tasksToGoalQuantifyDone={tasksToGoalQuantifyDone}
-            />
+      <main className='flex flex-col items-center'>
+        <Tab.Group>
+          <Tab.List className='w-11/12 mt-4'>
+            <div className='w-full flex flex-row gap-2'>
+              <Tab className='nav-btn'>
+                {({ selected }) => (
+                  /* Use the `selected` state to conditionally style the selected tab. */
+                  <button
+                    className={
+                      selected ? 'bg-[#5500C3] text-white' : 'bg-white'
+                    }
+                  >
+                    Objetivo
+                  </button>
+                )}
+              </Tab>
 
-            <TeamObjectivesNewTask 
-              message={message} 
-              handleSubmit={handleSubmit}
-              modelChange={modelChange}
-            />
+
+              <Tab className='nav-btn'>
+                {({ selected }) => (
+                  /* Use the `selected` state to conditionally style the selected tab. */
+                  <button
+                    className={
+                      selected ? 'bg-[#5500C3] text-white' : 'bg-white'
+                    }
+                  >
+                    Tarefas
+                  </button>
+                )}
+              </Tab>
+            </div>
+          </Tab.List>
+
+          <div className='w-11/12'>
+            <Tab.Panels>
+              <Tab.Panel>
+                <div className='flex flex-col items-center'>
+                  <h1 className='text-white text-bold text-2xl mt-2'> {tasksToGoalQuantify.nameGoal} </h1>
+                  <div className='container-two-percentage'>
+                    <TeamObjectivesPercentage
+                      tasksToGoalQuantify={tasksToGoalQuantify}
+                      tasksToGoalQuantifyDone={tasksToGoalQuantifyDone}
+                    />
+
+                    <TeamObjectivesNewTask
+                      message={message}
+                      handleSubmit={handleSubmit}
+                      modelChange={modelChange}
+                    />
+                  </div>
+
+                  <TeamObjectivesTable
+                    tasksToGoal={tasksToGoal}
+                    returnQuantify={returnQuantify}
+                    returnQuantifyDone={returnQuantifyDone}
+                    total={total}
+                    totalDone={totalDone}
+                  />
+                </div>
+              </Tab.Panel>
+
+              <Tab.Panel className='container-empresas'>
+                <TaskList/>
+              </Tab.Panel>
+            </Tab.Panels>
           </div>
-
-          <TeamObjectivesTable
-            tasksToGoal={tasksToGoal}
-            returnQuantify={returnQuantify}
-            returnQuantifyDone={returnQuantifyDone}
-            total={total}
-            totalDone={totalDone}
-          />
-        </div>
+        </Tab.Group>
       </main>
     </>
   );
