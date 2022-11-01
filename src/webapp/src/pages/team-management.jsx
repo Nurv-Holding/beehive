312d541e73c94e434b45f4ae0c8455cf7e0923f2
Nurv <gruponurv.ti@gmail.com>
2022-11-01 16:53:07 -0300

    up-teste

--- src/webapp/src/pages/team-management.jsx
+++ src/webapp/src/pages/team-management.jsx
@@ -4,11 +4,8 @@ import MenuTeamManagement from '../components/menuTeamManagement';
 function teamManagement() {
   return (
     <>
-<<<<<<< HEAD
-=======
       <Header />
 
->>>>>>> 34614f79587bbf4f274e6ed1afb9503a3a6c69a7
       <main className='flex flex-col items-center py-8'>
         <MenuTeamManagement/>
       </main>
