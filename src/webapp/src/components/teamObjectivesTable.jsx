5be0ba21c5ef5833812f5c8e12482bca0aa2b22e
Jefferson Dias <jefer.ld@hotmail.com>
2022-11-04 18:32:03 -0300

    weapp - closeModal function adjustment

--- src/webapp/src/components/teamObjectivesTable.jsx
+++ src/webapp/src/components/teamObjectivesTable.jsx
@@ -7,12 +7,10 @@ import goalKrsApi from "../api/goalKrsApi";
 function TeamObjectivesTable({ goalKrs }) {
   //Modal
   let [isOpen, setIsOpen] = useState(false)
-  const [goalKrName, setGoalKrName] = useState("")
   const [done, setDone] = useState(0)
+  const [goalKr, setGoalKr] = useState({})
   const [goalKrQuarterly, setGoalKrQuarterly] = useState("")
-  const [goalKryearly, setGoalKrYearly] = useState("")
   const [goalKrDone, setGoalKrDone] = useState(0)
-  const [idGoalsKr, setIdGoalsKr] = useState(null)
   const [message, setMessage] = useState("")
 
   function stateDone({target}) {
@@ -23,13 +21,8 @@ function TeamObjectivesTable({ goalKrs }) {
     setIsOpen(false)
   }
 
-  function openModal(id,name, quarterly, yearly, done) {
-    setIsOpen(true)
-    setGoalKrName(name)
-    setGoalKrQuarterly(quarterly)
-    setGoalKrYearly(yearly)
-    setGoalKrDone(done)
-    setIdGoalsKr(id)
+  function openModal(goalKr) {
+    setGoalKr(goalKr)
   }
 
   const goalKrsUpdate = (idGoalKrs,done) => {
@@ -47,7 +40,7 @@ function TeamObjectivesTable({ goalKrs }) {
     <>
       {(goalKrs || []).map((goalKr) => {
         return (
-          <div key={goalKr.id} onClick={() => openModal(goalKr.idgoalsKr,goalKr.nameGoalsKr, goalKr.QuarterlyGoalKrs, goalKr.yearlyGoalsKr, goalKr.doneGoalsKr)} className='flex flex-row items-center justify-around w-full bg-white p-4 mb-4 rounded-lg cursor-pointer'>
+          <div key={goalKr.id} onClick={() => openModal(goalKr)} className='flex flex-row items-center justify-around w-full bg-white p-4 mb-4 rounded-lg cursor-pointer'>
             <div className='flex items-center'>
               <span> {goalKr.nameGoalsKr} </span>
               <div className='w-3 h-3 ml-2 rounded-full bg-yellow-400 border border-black'></div>
@@ -68,30 +61,30 @@ function TeamObjectivesTable({ goalKrs }) {
         )
       })}
       <Modal isOpen={isOpen} closeModal={closeModal}>
-        <span className="text-lg uppercase"> {goalKrName} <span className="text-gray-600 text-xs">Atualizado: 04/11/22</span></span>
+        <span className="text-lg uppercase"> {goalKr.nameGoalsKr} <span className="text-gray-600 text-xs">Atualizado: 04/11/22</span></span>
 
         <div className="flex flex-col gap-[2%] mt-4">
           <div className="flex gap-2 items-center">
             <div>
               <input type="text" onChange={stateDone} className="input-style" name="done" placeholder="Atualizar os dados"/>
             </div>
-            <button onClick={() => {goalKrsUpdate(idGoalsKr)}} className="submit-button">ADD</button>
+            <button onClick={() => {goalKrsUpdate(goalKr.idgoalsKr, done)}} className="submit-button">ADD</button>
             <button className="submit-button">OK</button>
           </div>
           <div className="flex flex-col gap-[2%] mt-4">
-            <span>Meta Trimestral <span className="text-gray-600 text-xs">{goalKrQuarterly}</span></span>
+            <span>Meta Trimestral <span className="text-gray-600 text-xs">{goalKr.QuarterlyGoalKrs}</span></span>
             <div className='percentage-container-disclosure w-[90%] mt-2'>
               <div className='percentage-bar-disclosure w-[45%]'></div>
             </div>
-            <span className="text-gray-600 text-sm mt-2">Atual: {goalKrDone}</span>
+            <span className="text-gray-600 text-sm mt-2">Atual: {goalKr.QuarterlyGoalKrs}</span>
           </div>
 
           <div className="flex flex-col gap-[2%] mt-4">
-            <span>Meta Anual <span className="text-gray-600 text-xs">{goalKryearly}</span></span>
+            <span>Meta Anual <span className="text-gray-600 text-xs">{goalKr.doneGoalsKr}</span></span>
             <div className='percentage-container-disclosure w-[90%] mt-2'>
               <div className='percentage-bar-disclosure w-[45%]'></div>
             </div>
-            <span className="text-gray-600 text-sm mt-2">Atual: {goalKrDone}</span>
+            <span className="text-gray-600 text-sm mt-2">Atual: {goalKr.doneGoalsKr}</span>
           </div>
         </div>
       </Modal>
