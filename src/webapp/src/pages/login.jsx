e0eba15d332a2970e724f2070f82aeb2adc503bc
Higor Henrique <Higor Henrique>
2022-11-09 20:18:38 -0300

    Webapp - Tela login iniciada

---
+++ src/webapp/src/pages/login.jsx
@@ -0,0 +1,28 @@
+import Header from "../components/header"
+const Login = () => {
+    return (
+        <>
+            <Header />
+            <main className="flex flex-col items-center justify-center w-full ">
+                <div className="login-container">
+                    <h1 className="login-title">Entre em sua conta</h1>
+                    <form className="login-form-container">
+                        <div>
+                        <label htmlFor="">Usuário</label>
+                        <input className="input-style" type="text" placeholder="Escreva seu usuário"/>
+                        </div>
+              
+                        <div>
+                        <label htmlFor="">Senha</label>
+                        <input className="input-style" type="password" placeholder="Escreva sua senha" />    
+                        </div>
+                        
+                        <button className="submit-button" type= "submit">Entrar no portal</button>
+                    </form>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Login
