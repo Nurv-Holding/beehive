8c6c2ed3f2d2f63f14d40722e73652e79a674e31
Nurv <gruponurv.ti@gmail.com>
2022-12-20 14:41:24 -0300

    change goalslist

--- src/webapp/src/components/CompanyMenuPanel/Summary/GoalsList.jsx
+++ src/webapp/src/components/CompanyMenuPanel/Summary/GoalsList.jsx
@@ -22,20 +22,22 @@ const GoalsList = ({ companyGoals, goalKrs, goalAndTeams, krs }) => {
         <div className="mx-auto flex flex-row gap-4 flex-wrap mb-2">
             {(companyGoals || []).map((goal) => {
                 return(
-                    <div className="bg-slate-100 w-[500px] min-h-[300px] rounded-3xl shadow-lg p-2 flex flex-col items-center">
-                        <span className="text-[#5500C3] text-2xl font-bold">
+                    <div className="bg-slate-100 w-[500px] min-h-[300px] rounded-3xl shadow-lg py-4 px-6 flex flex-col items-center">
+                        <span className="text-[#5500C3] text-xl font-bold text-center uppercase">
                             <Link to={`goal/${goal.idGoal}`}>
                                 {goal.nameGoal}
                             </Link>
                         </span>
     
                         <span className="text-[#5500C3] text-xs mt-2 font-bold">Lista de Krs</span>
-                        <div className='flex flex-row'>
+                        <div className='grid grid-cols-2 gap-3 w-full'>
                             {(goalKrs || []).filter(e => e.idGoal === goal.idGoal).map((krs) => {
                                 return(
-                                    <div className="w-full flex m-1 flex-wrap text-sm justify-center">
-                                        <div className="bg-white p-6 rounded-xl text-black shadow-lg font-bold cursor-default">
-                                            {krs.name}
+                                    <div className="w-full">
+                                        <div className="bg-white w-full p-6 rounded-xl shadow-lg cursor-default">
+                                            <h1 className='text-black uppercase text-center font-bold text-[12px]'>
+                                                {krs.name}
+                                            </h1>
                                         </div>
                                     </div>
                                 )
@@ -46,24 +48,26 @@ const GoalsList = ({ companyGoals, goalKrs, goalAndTeams, krs }) => {
                             Times
                         </span>
     
-                        <div className="w-full flex gap-2 flex-wrap text-sm justify-center">
+                        <div className="w-full grid grid-cols-2 gap-3 text-sm justify-center">
                             {teams(goal.idGoal).map((team) => {
                                 return(
-                                    <div className="flex flex-col items-center gap-1 bg-white shadow-lg p-1 rounded-xl">
-                                        <div className="bg-white p-6 rounded-xl text-black shadow-lg font-bold cursor-default">
-                                            {team.nameTeam}
+                                    <div className="flex flex-col w-full items-center gap-1 bg-white shadow-lg p-3 rounded-xl">
+                                        <div className="bg-white p-6 rounded-xl shadow-lg cursor-default">
+                                            <h1 className='text-black font-bold uppercase text-[12px]'>
+                                                {team.nameTeam}
+                                            </h1>
                                         </div>
                                         {(goalAndTeams || []).filter(e => e.idTeam === team.idTeam && e.idGoal === team.idGoal).map((goalTeam) => {
                                             return(
                                                 <Disclosure>
-                                                    <Disclosure.Button className="bg-[#5500C3] p-3 rounded-xl text-xs text-white shadow-lg font-bold cursor-pointer">
+                                                    <Disclosure.Button className="bg-[#5500C3] py-2 px-3  uppercase rounded-xl text-[10px] text-white shadow-lg font-bold cursor-pointer">
                                                         {goalTeam.nameGoalTeam}
                                                     </Disclosure.Button>
                                                     {goalTeam.idGoalTeam &&
                                                     <>
                                                     {(krs || []).filter(e => e.idGoalTeam === goalTeam.idGoalTeam).map((kr) => {
                                                         return(
-                                                            <Disclosure.Panel className="bg-pink-500 p-2 text-xs rounded-xl text-black shadow-lg font-bold cursor-default">
+                                                            <Disclosure.Panel className="bg-pink-500 p-2 uppercase text-[10px] rounded-xl text-black shadow-lg font-bold cursor-default">
                                                                 {kr.nameGoalTeamKrs}
                                                             </Disclosure.Panel>
                                                         )
