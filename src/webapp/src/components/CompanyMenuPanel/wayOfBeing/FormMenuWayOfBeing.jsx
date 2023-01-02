f51a79c21fa7dbbe75140283f36175ab30162810
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2023-01-02 10:39:58 -0300

    webapp - Form Goals style solved

--- src/webapp/src/components/CompanyMenuPanel/wayOfBeing/FormMenuWayOfBeing.jsx
+++ src/webapp/src/components/CompanyMenuPanel/wayOfBeing/FormMenuWayOfBeing.jsx
@@ -118,12 +118,12 @@ function FormMenuWayOfBeing({ modelChange, message, handleSubmit }) {
                                         </span>
                                     </div>
 
-                                    <div className='flex flex-col w-2/4 items-center mt-4 bg-white p-2 rounded-lg shadow-xl'>
-                                        <form onSubmit={handleSubmit} className='w-full min-h-[300px] justify-center flex flex-col items-center'>
-                                            <div className='w-full gap-2 flex flex-wrap items-center justify-center'>
+                                    <div className='flex flex-col w-2/4 min-h-[300px] justify-center items-center mt-4 bg-white p-2 rounded-lg shadow-xl'>
+                                        <form onSubmit={handleSubmit} className='w-full flex flex-col items-center p-4 gap-4'>
+                                            <div className='w-[70%] flex flex-col items-center justify-center gap-4'>
                                                 <input type="text" required className="input-style text-center" placeholder='Título' onChange={modelChange} />
 
-                                                <textarea className="p-2 input-style min-h-[50px] text-center" placeholder='Descrição' name="note" onChange={modelChange} cols="60" rows="3"></textarea>
+                                                <textarea className="p-2 input-style min-h-[25px] text-center" placeholder='Descrição' name="note" onChange={modelChange} cols="50" rows="3"></textarea>
                                             </div>
 
                                             <button className='submit-button mt-4' type="submit">Cadastrar</button>
