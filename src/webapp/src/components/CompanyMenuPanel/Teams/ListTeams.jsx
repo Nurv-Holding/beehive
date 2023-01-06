4291c7842a1c92a3ada335030837d2bdc44629ba
Rafael Botelho <rafapereirabt@gmail.com>
2023-01-06 16:10:21 -0300

    webapp - list teams layout ajusted

--- src/webapp/src/components/CompanyMenuPanel/Teams/ListTeams.jsx
+++ src/webapp/src/components/CompanyMenuPanel/Teams/ListTeams.jsx
@@ -86,7 +86,7 @@ function ListTeams({ allTeams, users, teamsByKrs, teamByTeam, allTeamsAndUsers }
             return (
               <>
               <ModalMembersTeam idRef={"members"} idTeam={idTeam} users={users} />
-                <div className=' text-center bg-white p-2 flex flex-col items-center justify-center w-full aspect-square overflow-y-scroll rounded-3xl shadow-lg'>
+                <div className='text-center bg-white p-4 flex flex-col items-center w-full aspect-square overflow-y-scroll rounded-3xl shadow-lg'>
                   <div  className="cursor-pointer flex flex-col items-center justify-center">
                     <span className="text-bee-strong-1 text-xl font-bold text-center uppercase">
                       {team.nameTeam}
