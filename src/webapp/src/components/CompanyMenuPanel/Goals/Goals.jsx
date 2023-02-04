fca1766581dc2fc2bb96ea60ca06fb7629e535f1
Rafael Botelho <rafapereirabt@gmail.com>
2023-02-04 11:16:33 -0300

    webapp - accept

--- src/webapp/src/components/CompanyMenuPanel/Goals/Goals.jsx
+++ src/webapp/src/components/CompanyMenuPanel/Goals/Goals.jsx
@@ -4,44 +4,6 @@ import GoalsList from './GoalsList'
 
 function Goals() {
     const { companyGoals, goalAndTeams, krs, goalKrs } = useContext(ContextUser)
-<<<<<<< HEAD
-    // const [message, setMessage] = useState("Aqui vai uma mensagem")
-    // const navigate = useNavigate()
-    // const [searchParams, setSearchParams] = useSearchParams()
-    // const token = localStorage.getItem("token")
-    // const payload = token ? jwtDecode(token) : null
-
-    // const handleSubmit = (event) => {
-    //     event.preventDefault()
-
-    //     searchParams.delete('update')
-    //     setSearchParams(searchParams)
-
-    //     if (Object.values(item).length === 0 || item.name === "" || item.descriptions === "")
-    //         setMessage("Os campos precisam ser preenchidos")
-    //     else {
-
-    //         goalsApi.create(idCompany, { ...item, author: payload?.id })
-    //             .then(() => {
-    //                 setMessage("Cadastro realizado")
-    //                 navigate({
-    //                     pathname: `/company/${idCompany}`,
-    //                     search: `?update=${true}`
-    //                 })
-
-    //                 searchParams.delete("update")
-
-    //             })
-    //             .catch((error) => {
-    //                 console.error(error)
-    //                 setMessage(error)
-    //             })
-    //     }
-
-
-    // }
-=======
->>>>>>> 244e2115c2593744479eaade7a73edd5443bf19b
 
     return (
         <div className='w-full h-full flex flex-col items-center mt-8'>
