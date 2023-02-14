22ca477292a2aa6bcbd984e4bbd40ca0bd2558a7
Jefferson Dias <jefer.ld@hotmail.com>
2023-02-14 18:52:59 -0300

    webapp - usersApi adjustment

--- src/webapp/src/api/usersApi.jsx
+++ src/webapp/src/api/usersApi.jsx
@@ -1,17 +1,17 @@
 import axios from "axios"
 import { host } from "../utils/utilis"
 
-const options = {
-    source: '/:path*',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
-    },
-    // withCredentials: true,
-    // credentials: 'same-origin',
-  }
+// const options = {
+//     source: '/:path*',
+//     headers: {
+//       'Access-Control-Allow-Origin': '*',
+//       'Content-Type': 'application/json',
+//       'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+//       'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+//     },
+//     // withCredentials: true,
+//     // credentials: 'same-origin',
+//   }
 
 const getAllByCompany = (idCompany) => axios.get(`${host}/users/c/${idCompany}`)
 const getAll = () => axios.get(`${host}/users`)
