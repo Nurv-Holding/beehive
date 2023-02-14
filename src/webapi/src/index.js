6760b8e24b946de57dc121372eb7f6b3832f74cc
Jefferson Dias <jefer.ld@hotmail.com>
2023-02-14 14:16:48 -0300

    webapi - cors access adjustment

--- src/webapi/src/index.js
+++ src/webapi/src/index.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 3004
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
-    origin: '*'
+    origin: '*',
+    credentials: true,
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }))
 
 require('./routes')(app)
